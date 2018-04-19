b99f61716c434382cdd481ec1fa1f3a8c4ac7200
Döll, Sebastian <sebastian.doell@spring-media.de>
2018-04-19 12:33:50 +0200

    ~ adding error logging

--- webhook.go
+++ webhook.go
@@ -19,36 +19,47 @@ type WebHook struct {
 	Bot      string
 	URL      string
 	Type     string
+
+	log *log.Entry
 }
 
 // postPayload is posting a WebHookPayload
 func (w *WebHook) postPayload(webHookURL string, payload WebHookPayload) error {
 	var err error
 
-	// request log
-	logger := log.WithFields(log.Fields{
-		"webHookType": w.Type,
-	})
-
 	success, _, err := payload.Post(webHookURL) // omit response for now
 	if !success || err != nil {
-		logger.WithError(err)
 		return err
 	}
 
-	return err
+	return err // noop
 }
 
 // Send is sending a signal
 func (w *WebHook) Send(ctx context.Context, event e.CodePipelineEventDetail) error {
 	var err error
 
+	// setup logger
+	w.log = log.WithFields(log.Fields{
+		"type":     w.Type,
+		"pipeline": w.Pipeline,
+		"channel":  w.Channel,
+		"url":      w.URL,
+	})
+
+	// log
+	w.log.Info("Executing WebHook")
+
+	// configure the webhook
 	switch webhookType := w.Type; webhookType {
 	case WebHookTypeSlack:
 		slack := NewSlackPayload(w.Channel, w.Bot, event)
 		err = w.postPayload(w.URL, slack)
+		if err != nil {
+			w.log.WithError(err)
+		}
 	default:
 	}
 
-	return err
+	return err // noop
 }
